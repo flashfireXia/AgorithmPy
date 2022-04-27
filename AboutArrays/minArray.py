8b3d7de78c1c96fbba758aba94788f6451584503
wy <yuwang19940314@hotmail.com>
2022-04-28 00:04:36 +0800

    add

---
+++ AboutArrays/minArray.py
@@ -0,0 +1,23 @@
+'''
+剑指offer第11题：旋转数组的最小数字
+'''
+
+class Solution(object):
+    def minArray(self, nums):
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            elif nums[mid] < nums[left]:
+                right = mid
+            else:
+                return min(nums[left: right])
+        return nums[left]
+
+if __name__ == '__main__':
+    nums1 = [3, 4, 5, 1, 2]
+    nums2 = [2, 2, 2, 0, 1]
+    s = Solution()
+    print(s.minArray(nums1))
+    print(s.minArray(nums2))
